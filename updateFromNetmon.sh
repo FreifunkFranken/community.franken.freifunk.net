edba2c63b264be63402a5516396954a3cf9ae750
mojoaxel <alex@wunschik.net>
2014-11-01 16:11:43 +0100

    update all repositories befor making the update

--- updateFromNetmon.sh
+++ updateFromNetmon.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# get all nodes from netmon and update the community files
+# update all repositories
+cd ../freifunkfranken-community/
 git pull
+cd ../community.franken.freifunk.net/
+git pull
+
+# get all nodes from netmon and update the community files
 node ./create_community_files.js
 
 # update the github repository with the new data
