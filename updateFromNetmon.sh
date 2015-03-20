889716b7959dd4d80977c0eeb2d4f0f1ece5c008
mojoaxel <alex@wunschik.net>
2015-03-20 15:18:05 +0100

    fixed update script

--- updateFromNetmon.sh
+++ updateFromNetmon.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-BASEDIR=$(dirname $0)
+BASEDIR="$PWD"
+echo "BASEDIR: "."${BASEDIR}"
 
 # update community repository
-cd ${BASEDIR}/../freifunkfranken-community/
+cd "${BASEDIR}/../freifunkfranken-community/"
 git pull
 
 # get all nodes from netmon and update the community files
-cd ${BASEDIR}
+cd "${BASEDIR}"
 git pull
-node create_community_files.js
+node ./create_community_files.js 
 
 # update the github repository with the new data
-cd ${BASEDIR}/../freifunkfranken-community/
+cd "${BASEDIR}/../freifunkfranken-community/"
 git config user.name "NetmonUpdateScript"
 git commit -a -m "updated from netmon"
 git push origin master
+cd "${BASEDIR}"
 
